378935d634fbd05970a305f32de76effb5a3a0e6
Brian Steven Fierro Lechuga <brian.fierro-l@mail.escuelaing.edu.co>
2026-08-05 18:29:01 -0500

    Added the part of the TreeSet, finished the main and printing of the numbers

--- Laboratory1/challenge5/Challenge5.java
+++ Laboratory1/challenge5/Challenge5.java
@@ -1,6 +1,8 @@
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.Random;
+import java.util.TreeSet;
 
 public class Challenge5{
     // Organize the Hashset and filter it
@@ -11,8 +13,26 @@ public class Challenge5{
                                 .collect(Collectors.toCollection(HashSet::new));
         return result;
     }
+
+    private static TreeSet<Integer> CreationTree(){
+        TreeSet<Integer> tempTree = new TreeSet<>();
+        Random ran = new Random();
+
+        ran.ints(20, 0, 100).forEach(tempTree::add);
+
+        TreeSet<Integer> finalTree = tempTree.stream()
+                                            .filter(x -> x%5 != 0)
+                                            .collect(Collectors.toCollection(TreeSet::new));
+
+        return finalTree;
+    }
     public static void main(String[] args){
-        CreationHash();
+        HashSet<Integer> finalHash = CreationHash();
+        TreeSet<Integer> finalTree = CreationTree();
+
+        finalHash.stream().forEach(finalTree::add);
+
+        finalTree.stream().forEach(x -> System.out.println("Number in the arena:"+String.valueOf(x)));
 
     }
 }
