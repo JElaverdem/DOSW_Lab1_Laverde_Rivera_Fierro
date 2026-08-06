9a307934cb6d9fc13184bb094625f6df461505f3
Brian Steven Fierro Lechuga <brian.fierro-l@mail.escuelaing.edu.co>
2026-08-05 19:16:39 -0500

    Added the last four commands and the execution of them in main

--- Laboratory1/challenge6/Challenge6.java
+++ Laboratory1/challenge6/Challenge6.java
@@ -9,11 +9,28 @@ public class Challenge6{
             commands.put("FAREWELL", () -> System.out.println("May the bits be with you until the next mission"));
             commands.put("SING", () -> System.out.println("01010101"));
             commands.put("DANCE", () -> System.out.println("Spinning in party mode"));
+
+            commands.put("JOKE", () -> System.out.println("Why did the RAM break up with the CPU? It needed space."));
+            commands.put("SHOUT", () -> System.out.println("STACK OVERFLOW ALERT!"));
+            commands.put("WHISPER", () -> System.out.println("Shhh... the bugs are sleeping."));
+            commands.put("ANALYZE", () -> System.out.println("Processing data... result: You are amazing at programming!"));
             switch (command){
                 case"GREET":
                 case"FAREWELL":
                 case"SING":
                 case"DANCE":
+                    commands.get(command).run();
+                    break;
+                case "JOKE":
+                    commands.get(command).run();
+                    break;
+                case "SHOUT":
+                    commands.get(command).run();
+                    break;
+                case "WHISPER":
+                    commands.get(command).run();
+                    break;
+                case "ANALYZE":
                     commands.get(command).run();
                     break;
                 default:
@@ -22,5 +39,13 @@ public class Challenge6{
 
         }
             public static void main(String[] args){
-                
-    }}
+                executeCommand("GREET");
+                executeCommand("FAREWELL");
+                executeCommand("SING");
+                executeCommand("DANCE");
+                executeCommand("JOKE");
+                executeCommand("SHOUT");
+                executeCommand("WHISPER");
+                executeCommand("ANALYZE");
+        }
+}
