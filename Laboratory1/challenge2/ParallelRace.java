74dedac097826e0f028771506ea9f0c181cec6e1
Brian Steven Fierro Lechuga <brian.fierro-l@mail.escuelaing.edu.co>
2026-08-05 19:06:34 -0500

    initial commit, created the base class

---
+++ Laboratory1/challenge2/ParallelRace.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public class ParallelRace{
+
+
+
+    public static void main(string[] args){
+
+    }
+}
+/* 
+public class Results{
+    public int maximum;
+    public int minimum;
+    public int size;
+    public boolean isDivisor;
+    public boolean evenOdd;
+
+    public Results(int maximum, int minimum, int size, boolean isDivisor, boolean evenOdd){
+        this.maximum = maximum;
+        this.minimum = minimum;
+        this.size = size;
+        this.isDivisor = isDivisor;
+        this.evenOdd = evenOdd;
+    }
+}
+    */
